404de352a95017b4ef454a3b98e4e451ba752390
Michael Pearson <mpearson@ximasoftware.com>
2021-03-19 23:01:47 -0600

    Limit action sanitizer to Adapt actions

--- libs/core/src/lib/action-sanitizer.function.ts
+++ libs/core/src/lib/action-sanitizer.function.ts
@@ -2,15 +2,17 @@ import { Action } from '@ngrx/store';
 import { adaptType, PatchState } from './adapt.actions';
 
 export function actionSanitizer(action: Action & PatchState) {
-  return {
-    ...action,
-    action: undefined,
-    actionType: undefined,
-    source: undefined,
-    payload: action.source.payload,
-    type:
-      action.type === adaptType
-        ? `[${action.type}] ${action.source.type}`
-        : action.type,
-  };
+  return action.type === adaptType
+    ? {
+        ...action,
+        action: undefined,
+        actionType: undefined,
+        source: undefined,
+        payload: action.source.payload,
+        type:
+          action.type === adaptType
+            ? `[${action.type}] ${action.source.type}`
+            : action.type,
+      }
+    : action;
 }
