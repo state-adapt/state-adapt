100e3cd0786474a5e41a72643021e2e40a2fe760
Michael Pearson <mpearson@ximasoftware.com>
2021-04-18 04:06:09 -0600

    More React/StackBlitz experiments

--- scripts/prepare-react.js
+++ scripts/prepare-react.js
@@ -6,8 +6,7 @@ if (appName.includes('react')) {
   // Experimental React preparation
   rimraf('../angular.json', () => console.log('angular.json removed'));
   const appSrc = `../apps/${appName}/src`;
-  const appSrcFiles = fs.readdirSync(appSrc).map(dir => dir.name);
-  appSrcFiles.forEach(file => {
+  fs.readdirSync(appSrc).forEach(file => {
     const oldPath = `${appSrc}/${file}`;
     const newPath = `../${file}`;
     fs.rename(oldPath, newPath, () => console.log(`Moved ${oldPath} => ${newPath}`));
