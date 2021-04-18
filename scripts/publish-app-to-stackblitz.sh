05a3985c04d52456b13e1c10ba8ac61ccea5aecc
Michael Pearson <mpearson@ximasoftware.com>
2021-04-18 03:02:19 -0600

    Almost done with StackBlitz script

--- scripts/publish-app-to-stackblitz.sh
+++ scripts/publish-app-to-stackblitz.sh
@@ -5,6 +5,10 @@ node remove-dependencies.js;
 node make-app-alone-in-angular-json.js $1;
 node remove-other-apps.js $1;
 node make-lib-imports-relative.js $1;
+cd ../;
+git add . -A;
+# git commit -m "Publish for StackBlitz";
+# git push origin $1 -f;
 
 # git checkout main;
 # echo "didn't think this would run too, did you? $1"
