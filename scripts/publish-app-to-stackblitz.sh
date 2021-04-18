5b5b244d83e62e39271eaf69aa2fb76fd43daa09
Michael Pearson <mpearson@ximasoftware.com>
2021-04-18 03:19:54 -0600

    Mark the part to remove when StackBlitz supports nx

--- scripts/publish-app-to-stackblitz.sh
+++ scripts/publish-app-to-stackblitz.sh
@@ -2,10 +2,12 @@
 git checkout main;
 git branch -d $1;
 git checkout -b $1;
+# Prepare for Stackblitz
 node remove-dependencies.js;
 node make-app-alone-in-angular-json.js $1;
 node remove-other-apps.js $1;
 node make-lib-imports-relative.js $1;
+# / Prepare for Stackblitz
 cd ../;
 git add . -A;
 git commit -m "Publish for StackBlitz";
