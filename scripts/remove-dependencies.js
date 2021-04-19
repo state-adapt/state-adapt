7adc86e286bfe06cf64dffa90b105e4a9c83fd44
Michael Pearson <mpearson@ximasoftware.com>
2021-04-19 03:18:00 -0600

    Remove dependencies for now

--- scripts/remove-dependencies.js
+++ scripts/remove-dependencies.js
@@ -1,24 +1,7 @@
 const fs = require('fs');
 const packageJson = fs.readFileSync('../package.json', 'utf-8');
 const package = JSON.parse(packageJson);
-package.dependencies = {
-  '@angular/animations': '^11.2.10',
-  '@angular/common': '^11.2.10',
-  '@angular/compiler': '^11.2.10',
-  '@angular/core': '^11.2.10',
-  '@angular/forms': '^11.2.10',
-  '@angular/platform-browser': '^11.2.10',
-  '@angular/platform-browser-dynamic': '^11.2.10',
-  '@angular/router': '^11.2.10',
-  '@carbon/icons-angular': '^12.0.0',
-  '@state-adapt/core': '^0.35.0',
-  'carbon-components': '^10.33.0',
-  'carbon-components-angular': '^4.48.1',
-  'lodash': '^4.17.21',
-  'reselect': '^4.0.0',
-  'rxjs': '^6.6.7',
-  'zone.js': '^0.11.4',
-};
+package.dependencies = {};
 package.devDependencies = {};
 const newPackageJson = JSON.stringify(package, null, '  ');
 fs.writeFileSync('../package.json', newPackageJson);
