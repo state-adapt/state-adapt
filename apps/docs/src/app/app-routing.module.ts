2934d28aad962ea39e136f573a7ddcdd06941a3e
Michael Pearson <mpearson@ximasoftware.com>
2021-05-12 04:40:20 -0600

    docs: Enable anchor scrolling with offset

--- apps/docs/src/app/app-routing.module.ts
+++ apps/docs/src/app/app-routing.module.ts
@@ -82,7 +82,11 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+      scrollOffset: [0, 50],
+    }),
   ],
   exports: [RouterModule],
 })
