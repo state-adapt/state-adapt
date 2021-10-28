0c1ef199e9dbb52479bf760cd90cbda8fdd3476e
Michael Pearson <mpearson@ximasoftware.com>
2021-10-28 02:33:01 -0600

    Fix test

--- apps/docs/src/app/app.component.spec.ts
+++ apps/docs/src/app/app.component.spec.ts
@@ -16,18 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'docs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('docs');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to docs!',
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to docs!');
   });
 });
