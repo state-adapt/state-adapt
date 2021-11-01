e8bd4ff4b44806c69f89c7467d821a837a9c5c46
Michael Pearson <mpearson@ximasoftware.com>
2021-11-01 00:00:03 -0600

    Remove boilerplate from component store app

--- apps/ng-sa-component-store/src/app/app.component.spec.ts
+++ apps/ng-sa-component-store/src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'ng-sa-component-store'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-sa-component-store');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to ng-sa-component-store!',
-    );
-  });
 });
