59d3e51a0831bcc5fabf1e4353deb3660a12bafd
Michael Pearson <mfpears@gmail.com>
2022-01-18 19:31:58 -0700

    Run nx migrate

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/libs/core',
-    '<rootDir>/libs/ngrx',
-    '<rootDir>/libs/ngxs',
-    '<rootDir>/apps/docs',
-    '<rootDir>/libs/dashboards-feature',
-    '<rootDir>/apps/react-demo',
-    '<rootDir>/libs/react',
-    '<rootDir>/apps/ng-sa-counter',
-    '<rootDir>/libs/counter-ui',
-    '<rootDir>/apps/ng-sa-ngrx-counter',
-    '<rootDir>/apps/ng-sa-ngxs-counter',
-    '<rootDir>/libs/shopping',
-    '<rootDir>/apps/ng-sa-shopping',
-    '<rootDir>/libs/adapter-docs',
-    '<rootDir>/apps/ng-sa-component-store',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
