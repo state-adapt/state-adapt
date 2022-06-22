aa5183a62ad3a0fbe831be52a1d7d48ccfc9fa3b
Michael Pearson <mfpears@gmail.com>
2022-06-22 13:39:15 -0600

    feature(adapters): Improve type inference

--- libs/core/src/lib/create-adapter.function.ts
+++ libs/core/src/lib/create-adapter.function.ts
@@ -1,21 +1,17 @@
 import { Adapter, ReactionsWithSelectors } from './adapter.type';
 import { Selectors } from './selectors.interface';
 
-export interface BasicAdapterMethods<State> {
+export type BasicAdapterMethods<State> = {
   noop: (s: State) => State;
   set: (s: State, p: State) => State;
   update: (s: State, u: Partial<State>) => State;
   reset: (s: State, p: any, i: State) => State;
-}
+};
 
 export function createAdapter<State>() {
   return <S extends Selectors<State>, R extends ReactionsWithSelectors<State, S>>(
     adapter: Adapter<State, S, R> = {} as Adapter<State, S, R>,
-  ): Adapter<
-    State,
-    S,
-    R & BasicAdapterMethods<State> & ReactionsWithSelectors<State, S>
-  > => ({
+  ): Adapter<State, S, R & BasicAdapterMethods<State>> => ({
     noop: state => state,
     set: (state, payload) => payload,
     update: (state, update) => ({ ...state, ...update }),
