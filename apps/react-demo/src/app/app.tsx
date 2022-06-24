7e4030e3beb9b4b022242fddd604da85f66f16cb
Michael Pearson <mfpears@gmail.com>
2022-06-24 15:26:06 -0600

    feature(react): Use new init signature with React

--- apps/react-demo/src/app/app.tsx
+++ apps/react-demo/src/app/app.tsx
@@ -1,35 +1,131 @@
-import React from 'react';
-import { createAdapter } from '../../../../libs/core/src';
-import { useAdapter, useObservable, useSource } from '../../../../libs/react/src';
+import React, { useState } from 'react';
+import { interval } from 'rxjs';
+import { createAdapter, toSource } from '../../../../libs/core/src';
+import { useAdapt, useObservable, useSource } from '../../../../libs/react/src';
 
-const abAdapter = createAdapter<string>()({
-  concat: (state, newStr: string) => state + newStr,
+export const countAdapter = createAdapter<number>()({
+  increment: (state, n: number) => state + n,
+  double: state => state * 2,
+  selectors: {
+    bold: s => s.toString().bold(),
+  },
 });
 
 export function App() {
-  const click$ = useSource<{ test: boolean }>('[App] click$');
+  const resetAll$ = useSource<void>('[App] resetAll$');
+  const [interval$] = useState(() => interval(5000).pipe(toSource('interval$')));
 
-  const resetBoth$ = useSource<void>('[App] resetBoth$');
+  const store1 = useAdapt('count1', 0);
+  const store2 = useAdapt('count2', 0, interval$);
+  const store3 = useAdapt('count3', 0, countAdapter);
+  const store4 = useAdapt(['count4', 10], {
+    multiply: (state, n: number) => state * n,
+  });
+  const store5 = useAdapt('count5', 0, countAdapter, interval$);
+  const store6 = useAdapt(['count6', 0, countAdapter], {
+    set: interval$,
+    reset: resetAll$,
+  });
 
-  const store1 = useAdapter(['ab1', abAdapter, ''], { reset: resetBoth$ });
-  const ab1 = useObservable(store1.state$);
+  // store1.set();
+  // store2.set();
+  // store3.double(4);
+  // store4.multiply('4');
+  // store5.set('4');
+  // store6.increment();
 
-  const store2 = useAdapter(['ab2', abAdapter, ''], { reset: resetBoth$ });
-  const ab2 = useObservable(store2.state$);
+  const state1 = useObservable(store1.state$);
+  const state2 = useObservable(store2.state$);
+  const state3 = useObservable(store3.state$);
+  const bold3 = useObservable(store3.bold$);
+  const state4 = useObservable(store4.state$);
+  const state5 = useObservable(store5.state$);
+  const bold5 = useObservable(store5.bold$);
+  const state6 = useObservable(store6.state$);
+  const bold6 = useObservable(store6.bold$);
 
   return (
     <div>
       <main>
-        <button onClick={() => click$.next({ test: true })}>Make True</button>
-        <h1>ab1: {ab1}</h1>
-        <button onClick={() => store1.concat('a')}>a</button>
-        <button onClick={() => store1.concat('b')}>b</button>
-        <h1>ab2: {ab2}</h1>
-        <button onClick={() => store2.concat('a')}>a</button>
-        <button onClick={() => store2.concat('b')}>b</button>
+        <div>
+          <h2>Counter 1</h2>
+          <p>
+            {state1}
+            {/* <span dangerouslySetInnerHTML={{ __html: bold1 || '' }}></span> */}
+          </p>
+          <p>
+            <button onClick={() => store1.set(300)}>Set</button>
+            {/* <button onClick={() => store1.increment(3)}>Increment</button> */}
+            {/* <button onClick={() => store1.double()}>Double</button> */}
+          </p>
+        </div>
+
+        <div>
+          <h2>Counter 2</h2>
+          <p>
+            {state2}
+            {/* <span dangerouslySetInnerHTML={{ __html: bold2 || '' }}></span> */}
+          </p>
+          <p>
+            <button onClick={() => store2.set(300)}>Set</button>
+            {/* <button onClick={() => store2.increment(3)}>Increment</button> */}
+            {/* <button onClick={() => store2.double()}>Double</button> */}
+          </p>
+        </div>
+
+        <div>
+          <h2>Counter 3</h2>
+          <p>
+            {state3} <span dangerouslySetInnerHTML={{ __html: bold3 || '' }}></span>
+          </p>
+          <p>
+            <button onClick={() => store3.set(300)}>Set</button>
+            <button onClick={() => store3.increment(3)}>Increment</button>
+            <button onClick={() => store3.double()}>Double</button>
+          </p>
+        </div>
+
+        <div>
+          <h2>Counter 4</h2>
+          <p>
+            {state4}
+            {/* <span dangerouslySetInnerHTML={{ __html: bold4 || '' }}></span> */}
+          </p>
+          <p>
+            <button onClick={() => store4.set(300)}>Set</button>
+            <button onClick={() => store4.multiply(10)}>Multiply By 10</button>
+            {/* <button onClick={() => store4.increment(3)}>Increment</button> */}
+            {/* <button onClick={() => store4.double()}>Double</button> */}
+          </p>
+        </div>
+
+        <div>
+          <h2>Counter 5</h2>
+          <p>
+            {state5} <span dangerouslySetInnerHTML={{ __html: bold5 || '' }}></span>
+          </p>
+          <p>
+            <button onClick={() => store5.set(300)}>Set</button>
+            <button onClick={() => store5.increment(3)}>Increment</button>
+            <button onClick={() => store5.double()}>Double</button>
+          </p>
+        </div>
+
+        <div>
+          <h2>Counter 6</h2>
+          <p>
+            {state6} <span dangerouslySetInnerHTML={{ __html: bold6 || '' }}></span>
+          </p>
+          <p>
+            <button onClick={() => store6.set(300)}>Set</button>
+            <button onClick={() => store6.increment(3)}>Increment</button>
+            <button onClick={() => store6.double()}>Double</button>
+          </p>
+        </div>
+
         <br />
         <br />
-        <button onClick={() => resetBoth$.next()}>Reset Both</button>
+        <button onClick={() => resetAll$.next()}>Reset All</button>
       </main>
     </div>
   );
--- libs/react/src/index.ts
+++ libs/react/src/index.ts
@@ -1,6 +1,4 @@
 export * from './lib/adapt.context';
 export * from './lib/use-observable';
 export * from './lib/use-source';
-export * from './lib/use-adapter';
-export * from './lib/use-setter';
-export * from './lib/use-updater';
+export * from './lib/use-adapt';
