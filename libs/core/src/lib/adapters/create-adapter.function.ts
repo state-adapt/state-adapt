34eaabb4bae4d1940d3de598d15e65ae96881686
Michael Pearson <mfpears@gmail.com>
2023-01-04 15:14:26 -0700

    Refactor(core)!: Remove inappropriate update state change from createAdapter

--- libs/core/src/lib/adapters/create-adapter.function.ts
+++ libs/core/src/lib/adapters/create-adapter.function.ts
@@ -5,7 +5,7 @@ import { Adapter, ReactionsWithSelectors } from './adapter.type';
 export type BasicAdapterMethods<State> = {
   noop: (s: State) => State;
   set: (s: State, p: State) => State;
-  update: (s: State, u: Partial<State>) => State;
+  update: State extends object ? (s: State, u: Partial<State>) => State : never;
   reset: (s: State, p: void, i: State) => State;
 };
 
@@ -15,7 +15,9 @@ export function createAdapter<State>() {
   ): Adapter<State, S, R & BasicAdapterMethods<State>> => ({
     noop: state => state,
     set: (state, payload) => payload,
-    update: (state, update) => ({ ...state, ...update }),
+    update: ((state, update) => ({ ...state, ...update })) as State extends object
+      ? BasicAdapterMethods<State>['update']
+      : never,
     reset: (state, payload, initialState) => initialState,
     ...adapter, // New reactions object
     selectors: memoizeSelectors<State, S>((adapter.selectors as S) || ({} as S)), // New selectors object
