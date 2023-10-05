ecde9b5cc9158ec9be3f97ff750e9dcbfd75b825
Michael Pearson <mfpears@gmail.com>
2023-10-05 02:56:01 -0600

    Refactor(rxjs): Clean up generic name

--- libs/rxjs/src/lib/stores/selections.type.ts
+++ libs/rxjs/src/lib/stores/selections.type.ts
@@ -1,8 +1,8 @@
 import { Observable } from 'rxjs';
 import { Selectors } from '@state-adapt/core';
 
-export type Selections<State, T extends Selectors<State>> = {
-  [P in keyof T as `${P extends string ? P : never}$`]: Observable<
-    Exclude<ReturnType<T[P]>, undefined>
+export type Selections<State, S extends Selectors<State>> = {
+  [P in keyof S as `${P extends string ? P : never}$`]: Observable<
+    Exclude<ReturnType<S[P]>, undefined>
   >;
 } & { state$: Observable<State> };
