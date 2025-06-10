f0e8aa0c5d659e5b7b5ee2cffae478c0bcdf9503
Michael Pearson <mfpears@gmail.com>
2025-06-10 03:31:08 -0600

    WIP: Migrate to Nx 15.6

--- libs/react/jest.config.ts
+++ libs/react/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'react',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/react',
