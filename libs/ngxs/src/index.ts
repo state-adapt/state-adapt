6d2842c9ac4b3a44a3ec62a83f238291fb0c39bd
Michael Pearson <mfpears@gmail.com>
2025-12-08 01:42:54 -0700

    Refactor(ngxs)!: Remove direct NGXS support

--- libs/ngxs/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/adapt-ngxs.service';
-export * from './lib/adapt.state';
-export * from './lib/adapt-ngxs.function';
-export * from './lib/watch-ngxs.function';
-
-export * from './cheat-sheet.const';
--- libs/ngxs/src/lib/patch-state.action.ts
+++
@@ -1,6 +0,0 @@
-import { adaptType, Update } from '@state-adapt/core';
-
-export class PatchState {
-  static readonly type = adaptType; // NGXS requires static, NgRx requires NOT static
-  constructor(public actionType: string, public payload: Update[]) {}
-}
