c2297c8653a537acc0697304e5f4a03686be6804
Michael Pearson <mfpears@gmail.com>
2026-08-23 03:17:08 -0600

    chore: Remove old StackBlitz publish scripts

--- scripts/copy-built-styles-into-app.js
+++
@@ -1,5 +0,0 @@
-const appName = process.argv[2];
-const fs = require('fs');
-
-const styles = fs.readFileSync(`../dist/apps/${appName}/styles.css`, 'utf-8');
-fs.writeFileSync(`../apps/${appName}/src/styles.scss`, styles);
